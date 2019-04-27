8594f544eed089c4a0ce9d42039d743366e7dbd8
justynalezuch <justynalezuch@gmail.com>
2019-04-27 14:39:26 +0200

    Add CMarkup class

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <ctime>
+#include <cstdio>
+#include <cstdlib>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+
+#include "Markup.h"
 
 using namespace std;
 
+
+
 int main()
 {
+     CMarkup xml;
+    xml.AddElem( "INCOMES" );
+    xml.IntoElem();
+    xml.AddElem( "ITEM" );
+    xml.IntoElem();
+    xml.AddElem( "incomeId", 1 );
+    xml.AddElem( "userId", 3 );
+    xml.AddElem( "date", 3 );
+    xml.AddElem( "item", "AUTO" );
+    xml.AddElem( "amount", 15.14 );
+    xml.OutOfElem();
+
+
+
+    xml.Save("INCOMES.xml");
+
     cout << "Hello world!" << endl;
     return 0;
 }
